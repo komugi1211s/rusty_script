e6a34a27524ab565e3ff3f624ac78fe6c17fd6d2
fuzzyperson@home <5960126+Clutetot@users.noreply.github.com>
2020-02-05 23:46:14 +1000

    Still thinking about it

--- src/compiler/bytecode/mod.rs
+++ src/compiler/bytecode/mod.rs
@@ -7,35 +7,63 @@ use super::context::{ CodeGen, BranchMode, ConditionalBranch, Context };
 use super::typecheck::TypeArena;
 
 pub struct CompiledCode {
+    consts: Constants,
+}
+
+pub struct Constants {
+}
+
+impl Constants {
+    fn new() -> Self {
+        Self {}
+    }
+}
+
+// TODO - @Improvement: since Constants moves from Env to
+// CompiledCode at the end, I might want to box the entire
+// Structure of this
+pub struct Env {
+    defn:   TypeArena,
+    consts: Constants,
+}
+
+impl Env {
+    fn new() -> Self {
+        Env {
+            defn: TypeArena::new(),
+            consts: Constants::new(),
+        }
+    }
 }
 
 pub fn generate_bytecode(module: &SourceFile, ast: &ASTree) -> Result<CompiledCode, ()> {
     let mut ctx = Context::new();
-    let mut defn = TypeArena::new();
+    let mut env = Env::new();
 
     for node in ast.ast.iter() {
-        traverse_statement(&mut ctx, &mut defn, ast, node.stmt_id);
+        traverse_statement(&mut env, &mut ctx, ast, node.stmt_id);
     }
 
-    Ok(CompiledCode{})
+    Ok(CompiledCode {})
 }
 
 
 fn traverse_statement(
+    env: &mut Env,
     ctx: &mut impl CodeGen,
-    defn: &mut TypeArena,
     ast: &ASTree,
     statement_id: StmtId,
 ) {
     let statement = ast.get_stmt(statement_id);
 
     match statement {
-        Statement::Expression(expr_id) => traverse_expression(ctx, ast, *expr_id),
+        Statement::Expression(expr_id) => traverse_expression(env, ctx, ast, *expr_id),
         _ => unimplemented!(),
     };
 }
 
 fn traverse_expression(
+    env: &mut Env,
     ctx: &mut impl CodeGen,
     ast: &ASTree,
     expression_id: ExprId,
@@ -49,7 +77,9 @@ fn traverse_expression(
 }
 
 fn emit_literal(
+    env: &mut Env,
     ctx: &mut impl CodeGen,
     literal: &Literal
 ) {
+    
 }
